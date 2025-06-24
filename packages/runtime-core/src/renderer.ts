58f4b70e52ef8718c3c30f8243574211a423d0ec
huhe <hh646371951@gmail.com>
2025-06-24 23:48:08 +0800

    vue3-diff算法

--- packages/runtime-core/src/renderer.ts
+++ packages/runtime-core/src/renderer.ts
@@ -186,7 +186,116 @@ export function createRenderer(options) {
     }
   }
 
-  const patchKeyedChildren = (c1, c2, container) => {}
+  const patchKeyedChildren = (oldC, newC, container) => {
+    // 处理前置节点
+    let j = 0
+    let oldVNode = oldC[j]
+    let newVNode = newC[j]
+    while (oldVNode && newVNode && isSameVNodeType(oldVNode, newVNode)) {
+      patch(oldVNode, newVNode, container)
+      j++
+      oldVNode = oldC[j]
+      newVNode = newC[j]
+    }
+    // 处理后置节点
+    let oldEnd = oldC.length - 1
+    let newEnd = newC.length - 1
+    oldVNode = oldC[oldEnd]
+    newVNode = newC[newEnd]
+    while (
+      oldEnd >= j &&
+      newEnd >= j &&
+      oldVNode &&
+      newVNode &&
+      isSameVNodeType(oldVNode, newVNode)
+    ) {
+      console.log('chuli houzhi')
+      patch(oldVNode, newVNode, container)
+      oldEnd--
+      newEnd--
+      oldVNode = oldC[oldEnd]
+      newVNode = newC[newEnd]
+    }
+    console.log(j, oldEnd, newEnd)
+    if (j > oldEnd && j <= newEnd) {
+      // 旧节点处理完 存在多的新节点 要新增
+      console.log('add')
+      let idx = newEnd
+      let addAnchor = newC[idx + 1].el
+      while (idx >= j) {
+        patch(null, newC[idx--], container, addAnchor)
+        addAnchor = newC[idx + 1].el
+      }
+    } else if (j > newEnd && oldEnd >= j) {
+      // 新节点处理完 存在多的旧节点 要删除 j - oldEnd
+      console.log('delete')
+      while (j <= oldEnd) {
+        unmount(oldC[j++])
+      }
+    } else {
+      // 乱序情况
+      console.log('j', j, newEnd, oldEnd)
+      // 还没处理的新节点的key和index的map
+      const keyToNewIndexMap = new Map()
+      // 记录newC中没有处理的节点 在oldC中的位置
+      const newIndexToOldIndexMap = new Array(newEnd - j + 1).fill(-1)
+      console.log(newIndexToOldIndexMap)
+
+      for (let i = j; i <= newEnd; i++) {
+        const n2 = newC[i]
+        keyToNewIndexMap.set(n2.key, i)
+      }
+      console.log(keyToNewIndexMap)
+
+      let pos = -1
+      let moved = false
+
+      // 遍历老节点 找到可以复用的
+      for (let i = j; i <= oldEnd; i++) {
+        const n1 = oldC[i]
+        const newIndex = keyToNewIndexMap.get(n1.key)
+        if (newIndex !== undefined) {
+          if (newIndex > pos) {
+            // 通过老节点找到新节点的位置 如果新节点的位置是递增的 则说明此时不需要移动
+            pos = newIndex
+          } else {
+            // 若不是递增则说明要移动
+            moved = true
+          }
+          newIndexToOldIndexMap[newIndex] = i
+          patch(n1, newC[newIndex], container)
+        } else {
+          // 没找到卸载
+          console.log('not found delete')
+          unmount(n1)
+        }
+      }
+      console.log(moved)
+      console.log(newIndexToOldIndexMap)
+      // 如果需要移动则计算最长递增子序列
+      const newIndexSeq = moved ? getSeq(newIndexToOldIndexMap) : []
+      const set = new Set(newIndexSeq)
+
+      /**
+       * 遍历newC 倒序插入
+       */
+      for (let i = newEnd; i >= j; i--) {
+        const n2 = newC[i]
+        const anchor = newC[i + 1]?.el || null
+        if (n2.el) {
+          if (moved) {
+            // 在最长递增子序列中则需要移动
+            if (!set.has(i)) {
+              console.log('need move')
+              hostInsert(n2.el, container, anchor)
+            }
+          }
+        } else {
+          patch(null, n2, container, anchor)
+        }
+      }
+    }
+  }
 
   const patchElement = (n1, n2) => {
     /**
@@ -229,5 +338,59 @@ export function createRenderer(options) {
 
   return {
     render,
+  }
+}
+
+function getSeq(arr) {
+  const result = []
+  // 记录前驱节点
+  const map = new Map()
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i]
+    if (item === -1 || item === undefined) continue
+
+    if (result.length === 0) {
+      result.push(i)
+      continue
+    }
+
+    const lastIndex = result[result.length - 1]
+    const lastItem = arr[lastIndex]
+
+    if (item > lastItem) {
+      // 大于上一个直接放入result
+      result.push(i)
+      map.set(i, lastIndex)
+      continue
+    }
+
+    let left = 0
+    let right = result.length - 1
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2)
+      const midItem = arr[result[mid]]
+      if (midItem < item) {
+        left = mid + 1
+      } else {
+        right = mid - 1
+      }
+    }
+
+    result[left] = i
+    map.set(i, result[left - 1])
+
+    // 反向追溯
+    let l = result.length
+    let last = result[l - 1]
+
+    while (l > 0) {
+      l--
+      // 纠正顺序
+      result[l] = last
+      // 去前驱节点里面找
+      last = map.get(last)
+    }
+
+    return result
   }
 }
